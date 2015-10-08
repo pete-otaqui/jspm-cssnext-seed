d47a1612e5e61dfa741d8a8cb7fe8d585b2f1c02
Pete Otaqui <pete@otaqui.com>
2015-10-08 12:48:44 +0100

    moved lib/ to js/, added some content to index page, renamed

--- npm-scripts/styles.js
+++ npm-scripts/styles.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 /*eslint-env node */
-var fs = require("fs");
-var cssnext = require("cssnext");
+var fs = require('fs');
+var cssnext = require('cssnext');
 
 var path = './styles/';
 var mainCssFile = 'main.css';
 var bundleCssFile = 'bundle.css';
 
-var input = fs.readFileSync(path + mainCssFile, "utf8");
+var input = fs.readFileSync(path + mainCssFile, 'utf8');
 
 var output = cssnext(input, {
   from: path + mainCssFile,
@@ -17,7 +17,7 @@ var output = cssnext(input, {
   map: {inline: false}
 });
 
-//  {css: "{css string}", map: {sourcemap object}}
+//  {css: '{css string}', map: {sourcemap object}}
 
 fs.writeFileSync(path + bundleCssFile, output.css);
 fs.writeFileSync(path + bundleCssFile + '.map', output.map);
